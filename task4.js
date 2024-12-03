4cc4c3043c912be85563fc6a71114e9a3adc582c
ashina <uT_4011@zsp3zamosc.pl>
2024-12-03 08:24:24 +0100

    first task

---
+++ task4.js
@@ -0,0 +1,4 @@
+let firstNumber = 351;
+let secondNumber = 531;
+
+console.log(`Większą z dwóch liczb jest: ${Math.max(firstNumber,secondNumber)}  a mniejsza to ${Math.min(firstNumber,secondNumber)}`);
